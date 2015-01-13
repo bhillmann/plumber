cebbd0beba0c001e0c9363f14da250b918d96de3
Benjamin Hillmann <bhillmann@outlook.com>
2015-01-13 16:22:16 -0600

    The ReducePipe now works correctly

--- lib/Plumber/Pipe/ReducePipe.php
+++ lib/Plumber/Pipe/ReducePipe.php
@@ -10,13 +10,16 @@ namespace Plumber\Pipe;
 
 // TODO: In the future, will be able to allow custom reduce functions
 // The custom function would look for a key for each value and emit the new key
-class ReducePipe extends TransformPipe {
+use Plumber\Pipe;
 
-    public function __construct() {
-        parent::construct(array($this, 'reduce'));
-    }
+class ReducePipe extends Pipe {
+    protected $reduced = [];
 
-    public function reduce($value, $key) {
-        return self::drain();
+    public function current() {
+        while (parent::valid()) {
+            $this->reduced[] = parent::current();
+            parent::next();
+        }
+        return $this->reduced;
     }
 }
